6d2decf635cce21ca94944ed99b5dd4bf62ec29c
hamsterderbo <38465093+hamsterderbo@users.noreply.github.com>
2018-05-19 09:31:56 +0200

    Update TeamQuattro_.java

--- production/TeamQuattro_.java
+++ production/TeamQuattro_.java
@@ -30,13 +30,13 @@ public class TeamQuattro_ implements PlugIn {
 			"A9_7", "A9_8", "A9_9", "A9_10", "A9_11", "A9_12", "B1_1", "B1_2", "B2_1", "B2_3", "B3_2", "B3_3", "B4_1",
 			"B5_2", "B6_3", "PEI09" };
 
-	// Dialog um Eingaben vom Nutzer bzgl. der Blockgröße einzuholen
+	// Dialog um Eingaben vom Nutzer bzgl. der BlockgrÃ¶ÃŸe einzuholen
 	public static void addDialogue() {
 
 		// Nutzen des in ImageJ vorgefertigten "GenericDialog"
 		GenericDialog dialog = new GenericDialog("Enter the number of blocks!");
 
-		// HinzufÃ¼gen von Eingabefeldern
+		// HinzufÃƒÂ¼gen von Eingabefeldern
 		dialog.addNumericField("Number of blocks in a row: ", 2, 0);
 		dialog.addNumericField("Number of blocks in a column: ", 2, 0);
 
@@ -51,7 +51,7 @@ public class TeamQuattro_ implements PlugIn {
 		// Anzeigen des Dialoges
 		dialog.showDialog();
 
-		// Prüfen ob Dialog abgebrochen wurde
+		// PrÃ¼fen ob Dialog abgebrochen wurde
 		if (dialog.wasCanceled()) {
 			return ;
 		}
@@ -122,32 +122,7 @@ public class TeamQuattro_ implements PlugIn {
 			}
 		}
 		 
-		
-		
-//			
-//			if(i % wide == 1){  
-//				
-//				functions.iprun(stack.getProcessor(i+1), mode , sigen[(i+1)-(wide-1)],true);
-//				
-////				if(i > sigen.length/wide){
-////				
-////				
-////				ips[0].insert(ipstemp[0], w/wide*i, 0);
-////				ips[1].insert(ipstemp[1], w/wide*i, 0);
-////				ips[2].insert(ipstemp[2], w/wide*i, 0);
-//				
-//				
-//			} else {
-//				functions.iprun(stack.getProcessor(i+1), mode , sigen[i-1],true);
-//				
-////				ips[0].insert(ipstemp[0], w/wide*i, 0);
-////				ips[1].insert(ipstemp[1], w/wide*i, 0);
-////				ips[2].insert(ipstemp[2], w/wide*i, 0);
-//				
-//			}
-//			
-//		}
-		
+				
 		imps[0].show();
 		imps[1].show();
 		imps[2].show();
@@ -157,7 +132,7 @@ public class TeamQuattro_ implements PlugIn {
 
 	public void decode(int w, int h){
 				
-		// Alle Bilder zerteilen und je Block in einem Array zusammenführen
+		// Alle Bilder zerteilen und je Block in einem Array zusammenfÃ¼hren
 		
 		ImagePlus imps = null;
 		
@@ -168,14 +143,13 @@ public class TeamQuattro_ implements PlugIn {
 		ImageProcessor[] tempimg = new ImageProcessor[1];
 		
 		ImageProcessor[] ips = new ImageProcessor[1];
-		//ips[0] = new FloatProcessor(w, h);
 		ips[0] = new ColorProcessor(w, h);
 		imps = new ImagePlus("Endresultat : Spaghetti", ips[0]);
 		
 		tempimg[0] = new ColorProcessor(w/wide, h/high);
 		
 
-		// Funktion die das Bild zerteilt und die Blöcke auf einen Stack legt
+		// Funktion die das Bild zerteilt und die BlÃ¶cke auf einen Stack legt
 		ImageStack stack1 = functions.createStack(img1.getProcessor(), wide, high);
 		ImageStack stack2 = functions.createStack(img2.getProcessor(), wide, high);
 		ImageStack stack3 = functions.createStack(img3.getProcessor(), wide, high);
@@ -183,20 +157,16 @@ public class TeamQuattro_ implements PlugIn {
 		int yvu1[][][];
 		int yvu2[] = {0,0,0};
 		
+		// Signalenergien speichern
+		String sigenergy[] = new String[wide*high];  //Signalwerte speichern
+		
 		int xins= 0;
 		int yins= 0;
 		int counter= 0;
 		
 		for(int z = 1 ; z <= wide*high ; z ++){
 			
-
-			
-			String rctdec = null;
-				
-			
-			
-			// WELCHER RCT WIRD GENUTZT
-			
+			String[] rctdec = null;
 			
 			for(int x = 0 ; x < w/wide ; x++){
 				for(int y= 0 ; y < h/high ; y++){
@@ -210,13 +180,16 @@ public class TeamQuattro_ implements PlugIn {
 					if(z == 1){
 						yvu1 = functions.iprun(stack1.getProcessor(z), "dec", "A7_1");
 					} else {
-						yvu1 = functions.iprun(stack1.getProcessor(z), "dec", "A7_1");
+						
+						System.out.println("FEHLER  :  SIGENERGY =  " + (z-2)  + "   "+ sigenergy[z-2]);
+						
+						yvu1 = functions.iprun(stack1.getProcessor(z), "dec", sigenergy[z-2]);
 					}		
 										
 					yvu2[0] = yvu1[x][y][0];
 					yvu2[1] = yvu1[x][y][1];
 					yvu2[2] = yvu1[x][y][2];
-									
+
 					
 					tempimg[0].putPixel(x, y, yvu2);
 				
@@ -224,6 +197,47 @@ public class TeamQuattro_ implements PlugIn {
 				}	
 				
 			}
+			
+			
+	// WELCHER RCT WIRD GENUTZT ***********************
+			
+			int sigtemp = 0;
+			int sigact = 0;
+			
+			// Jede RCT
+			for (int q = 0; q < rcts.length; q++) {
+
+				System.out.println("***************  Abschnitt : " + z + "     Fange an mit RCT " + rcts[q]);
+				System.out.println("BEST : " + sigenergy[z-1]);
+
+				
+				int yvu[][][] = functions.iprun(tempimg[0], "enc", rcts[q]);
+						
+				// Signalenergie per Pixel im Block , Bester wird gespeichert in sigenergy[]
+				for (int i = 0; i < yvu.length; i++) {
+					for (int y = 0; y < yvu[0].length; y++) {
+					
+						sigact = (yvu[i][y][0]*yvu[i][y][0])+(yvu[i][y][1]*yvu[i][y][1])+(yvu[i][y][2]*yvu[i][y][2]);
+						
+						if( sigtemp == 0){
+							sigtemp = sigact;
+						} else if( sigact < sigtemp){
+							
+							System.out.println("SWAP : "  + sigtemp + " = " + sigact);
+							
+							sigtemp = sigact;
+
+							sigenergy[z-1] = rcts[q];
+						}
+					}
+				}
+				
+				System.out.println("Block " + z + "    Beste RCT :  "  +  sigenergy[z-1] );
+
+			}
+				
+			
+	// END   WELCHER RCT WIRD GENUTZT ***********************
 			
 			ips[0].insert(tempimg[0], w/wide*xins, h/high*yins);
 			
@@ -238,12 +252,9 @@ public class TeamQuattro_ implements PlugIn {
 				
 			}			
 			
-			
 		}
-		
 	
-		imps.show();
-			
+		imps.show();		
 
 	}
 
@@ -254,7 +265,7 @@ public class TeamQuattro_ implements PlugIn {
 		// Test ob ein Bild vorhanden ist
 		functions.failcheck();
 		
-		// Dialog um Eingaben vom Nutzer bzgl. der Blockgröße einzuholen
+		// Dialog um Eingaben vom Nutzer bzgl. der BlockgrÃ¶ÃŸe einzuholen
 		addDialogue();
 		
 		
@@ -263,14 +274,14 @@ public class TeamQuattro_ implements PlugIn {
 		if( mode == "enc"){
 			ImagePlus iplus = WindowManager.getCurrentImage();
 			
-			// Ungerade Bildmaße abfangen
+			// Ungerade BildmaÃŸe abfangen
 			if(iplus.getWidth()%wide > 0 || iplus.getHeight()%high > 0 ){
-					IJ.showMessage("Bitte anderes Bild wählen!");
+					IJ.showMessage("Bitte anderes Bild wÃ¤hlen!");
 					return;
 			}
 
 			// Anzeige: new ImagePlus("Blocks", stack).show();
-			// Funktion die das Bild zerteilt und die Blöcke auf einen Stack legt
+			// Funktion die das Bild zerteilt und die BlÃ¶cke auf einen Stack legt
 			ImageStack stack = functions.createStack(iplus.getProcessor(), wide, high);
 
 			// Signalenergien speichern
@@ -278,8 +289,8 @@ public class TeamQuattro_ implements PlugIn {
 
 			
 			
-			// BESTE RCT FÜR JEDEN BLOCK ERMITTELN
-			// Für alle Bildabschnitte
+			// BESTE RCT FÃœR JEDEN BLOCK ERMITTELN
+			// FÃ¼r alle Bildabschnitte
 			for (int x = 1; x <= stack.getSize(); x++) {
 				
 				int sigtemp = 0;
@@ -325,41 +336,41 @@ public class TeamQuattro_ implements PlugIn {
 			}
 			
 			
-		// Rücktransformation
+		// RÃ¼cktransformation
 		} else {
 			
 			
 			ImagePlus iplus = WindowManager.getCurrentImage();
 			
-			// 3 Bilder bei Rücktransformation vorhanden? 
+			// 3 Bilder bei RÃ¼cktransformation vorhanden? 
 			if(WindowManager.getImageCount() < 3){
-				IJ.showMessage("Bitte drei Bilder auswählen!");
+				IJ.showMessage("Bitte drei Bilder auswÃ¤hlen!");
 				return;
 			}
 			
-			// Bildgröße prüfen
+			// BildgrÃ¶ÃŸe prÃ¼fen
 			if(WindowManager.getImage(1).getHeight() != WindowManager.getImage(2).getHeight() 
 					|| WindowManager.getImage(1).getHeight() != WindowManager.getImage(3).getHeight()
 					|| WindowManager.getImage(2).getHeight() != WindowManager.getImage(3).getHeight() ){
-				IJ.showMessage("Bitte drei gleich große Bilder auswählen!");
+				IJ.showMessage("Bitte drei gleich groÃŸe Bilder auswÃ¤hlen!");
 				return;
 			}
 			
 			if(WindowManager.getImage(1).getWidth() != WindowManager.getImage(2).getWidth() 
 					|| WindowManager.getImage(1).getWidth() != WindowManager.getImage(3).getWidth()
 					|| WindowManager.getImage(2).getWidth() != WindowManager.getImage(3).getWidth() ){
-				IJ.showMessage("Bitte drei gleich große Bilder auswählen!");
+				IJ.showMessage("Bitte drei gleich groÃŸe Bilder auswÃ¤hlen!");
 				return;
 			}
 			
 			
-			// Funktion die das Bild zerteilt und die Blöcke auf einen Stack legt
+			// Funktion die das Bild zerteilt und die BlÃ¶cke auf einen Stack legt
 			ImageStack stack = functions.createStack(iplus.getProcessor(), wide, high);
 			
 			
-			// Ungerade Bildmaße abfangen
+			// Ungerade BildmaÃŸe abfangen
 			if(iplus.getWidth()%wide > 0 || iplus.getHeight()%high > 0 ){
-					IJ.showMessage("Bitte anderes Bild wählen!");
+					IJ.showMessage("Bitte anderes Bild wÃ¤hlen!");
 					return;
 			}
 			
